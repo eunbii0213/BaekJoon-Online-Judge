0d96c44c3234dcc65e978ef2b4a72ed29825a389
eunbii0213 <67318165+eunbii0213@users.noreply.github.com>
2020-09-22 00:37:31 +0900

    Create 14681.cpp

---
+++ 14681.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+
+	int x,y;
+	cin >> x >> y;
+
+	if (x > 0 && y > 0) {
+		cout << "1";
+	}
+
+	else if (x > 0 && y < 0) {
+		cout << "4";
+	}
+	else if (x < 0 && y < 0) {
+		cout << "3";
+	}
+
+	else  {
+		cout << "2";
+	}
+}
