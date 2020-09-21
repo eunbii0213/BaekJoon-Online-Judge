affd1b60c7257c9d231cf6502c0e36356f35236f
eunbii0213 <67318165+eunbii0213@users.noreply.github.com>
2020-09-22 00:33:30 +0900

    Create 1008.cpp

---
+++ 1008.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+	using namespace std;
+
+	int A, B;
+	
+
+	cin >> A >> B;
+	printf("%.10f", (double)A / (double)B);
+	
+	}
