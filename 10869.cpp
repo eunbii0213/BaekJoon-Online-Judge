139f27d13b77f0ba9de1875e711ddfe360ff8d73
eunbii0213 <67318165+eunbii0213@users.noreply.github.com>
2020-09-22 00:33:58 +0900

    Create 10869.cpp

---
+++ 10869.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+	using namespace std;
+
+	int A, B;
+	
+
+	cin >> A >> B;
+	cout << A + B << endl;
+	cout << A - B << endl;
+	cout << A * B << endl;
+	cout << A / B << endl;
+	cout << A % B << endl;
+
+	
+	
+	}
