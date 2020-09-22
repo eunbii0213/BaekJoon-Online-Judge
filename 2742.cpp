63f524c532b2d6e1e9732b40673bdd6fb2e6198d
은비츄 <67318165+eunbii0213@users.noreply.github.com>
2020-09-22 10:12:11 +0900

    Create 2742.cpp

---
+++ 2742.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+
+
+
+using namespace std;
+
+int main() {
+
+
+
+	int N;
+	
+	cin>>N; //테스트의 개수 입력
+
+	for (int i = N; i >= 1; i--) {
+		
+		cout << i <<"\n";
+
+	}
+	
+	
+}
