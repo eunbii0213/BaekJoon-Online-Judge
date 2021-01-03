63dc51e3a287fa9019878e0d4491d90eedf4e7ba
은비츄 <67318165+eunbii0213@users.noreply.github.com>
2021-01-03 18:13:16 +0900

    Create 11654.c

---
+++ 11654.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <stdio.h>
+
+int main()
+{
+	char ch;
+	scanf("%c", &ch);//문자열로 입력받고
+
+	printf("%d", ch); //정수형으로 출력하면 아스키값 나옴
+}
+
+
