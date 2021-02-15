6081a26f4810c28e9f96921437ec7bf1ce992943
은비츄 <67318165+eunbii0213@users.noreply.github.com>
2021-02-15 16:21:27 +0900

    Create 1085.java

---
+++ 1085.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main{
+
+		 
+		public static void main(String[] args){
+			Scanner scanner = new Scanner(System.in);
+			
+			int x= scanner.nextInt();//한수가 서있는 가로 위치
+			int y= scanner.nextInt();//한수가 서있는 세로 위치
+			int w= scanner.nextInt();//직사각형의 가로 길이
+			int h= scanner.nextInt();//직사각형의 세로 길이
+			//x,y,w,h 입력 받음 
+			
+			int one = h-y;
+			int two = w-x;
+			int three = x;
+			int four = y;
+			
+			int min=100000;
+			
+			if(one<min && one>=0) {
+				min=one;
+			}
+		
+			if(two<min && two>=0) {
+				min=two;
+			}
+			
+			if(three<min && three>=0) {
+				min=three;
+			}
+		
+
+			if(four<min && four>=0) {
+				min=four;
+				}
+		
+			
+			int five = (int) Math.sqrt((two*two)+(one*one));
+			
+			int six = (int) Math.sqrt((x*x)+(y*y));
+			
+			
+			if(five<min && five>=0) {
+				min=five;
+			}
+			
+			if(six<min && six>=0) {
+				min=six;
+			}
+			
+			
+			System.out.println(min);
+			
+			
+			
+	}
+
+}
