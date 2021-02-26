cbd2470b373ac08e86b8d178c7f59694d37cb88b
은비츄 <67318165+eunbii0213@users.noreply.github.com>
2021-02-27 08:49:00 +0900

    Create 2609.java

---
+++ 2609.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		
+		int a = in.nextInt();
+		int b = in.nextInt();
+ 
+		int d = gcd(a, b);	// 최대공약수
+ 
+		System.out.println(d);
+		System.out.println(a * b / d);
+ 
+	}
+ 
+	// 최대공약수 반복문 방식
+	public static int gcd(int a, int b) {
+ 
+		while (b != 0) {
+			int r = a % b; // 나머지를 구해준다.
+ 
+			// GCD(a, b) = GCD(b, r)이므로 변환한다.
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+}
