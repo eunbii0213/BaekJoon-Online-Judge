1222c5b75b59de81e0fc76c98aa3a80a801349b5
은비 <67318165+eunbii0213@users.noreply.github.com>
2021-07-04 01:14:23 +0900

    Update 15650.java

--- 15650.java
+++ 15650.java
@@ -4,45 +4,66 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main{
 	
-	static int[] arr;
-	
+	static StringBuilder sb = new StringBuilder();
+	static int arr[]; //1 2 3 4 같은 수 담을 배열
+	static boolean visit[]; //다 false로 초기화 되어있음
 	static int N;
 	static int M;
+	static int index_number;
+	//N>=M
 	
-	public static void dfs(int at, int depth) {
+	public static void dfs(int depth, int index_number) {
 		
 		if(depth == M) {
-			for(int val : arr) {
-				System.out.print(val+" ");
+			
+			for(int i=0;i<M;i++) {
+				
+				sb.append(arr[i]).append(' ');
+			
 			}
-			System.out.println("");
+			
+			sb.append("\n");
+			
 			return;
-		}
+		} //종료조건
 		
-		
-			for(int i=at; i<=N; i++) {
+		for(int i=index_number; i<N; i++) {
+			
+			if(!visit[i]) {
 				
-					arr[depth] = i;
-					dfs(i+1,depth+1);
+				visit[i]=true; //방문했음
+				arr[depth]=i+1; //arr채워넣음 
 				
-		}
+				index_number=i+1;//썼던 숫자 뛰어넘기위함
+				dfs(depth+1,index_number);
+				
+				visit[i]=false; //초기화
+			}//방문 안 했다면
+		} //for문 끝. 방문 안 한 곳만 들린다(중복x)
 	}
 	
-
+	
+	
+	
 public static void main(String[] args) throws IOException {
 		
-		Scanner scanner = new Scanner(System.in);
 		
-		N = scanner.nextInt();
-		M = scanner.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		
-		arr = new int[M];
+		 N = Integer.parseInt(st.nextToken());
+		 M = Integer.parseInt(st.nextToken());
 		
-		dfs(1,0);
-
+		arr = new int[M]; // M갯수만큼 넣고 출력하고를 반복할것임
+		visit = new boolean[N]; // N개만큼 필요(중복값확인배열)
+		index_number=0;
+		
+		dfs(0,0);
+		System.out.print(sb);
 	}
 
 }
