de35c80c70a02fee1b71dc046f63f2aa6f2b2f60
은비 <67318165+eunbii0213@users.noreply.github.com>
2021-07-19 15:46:18 +0900

    Update 2164(2).java

--- 2164(2).java
+++ 2164(2).java
@@ -16,35 +16,31 @@ public static void main(String[] args) throws IOException {
 	
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	int N = Integer.parseInt(br.readLine());
-	PriorityQueue<Integer> queue = new PriorityQueue<>();
-	Queue<Integer> q = new LinkedList<>();
+	Queue<Integer> queue = new LinkedList<>();
+	
 	
 	for(int i=1;i<(N+1);i++) {
 		queue.add(i);
-	}//오름차순 
-	
-	for(int i=0;i<N;i++) {
-		q.add(queue.peek());
-		queue.remove();
-	}//일반 큐에 옮겨담음 (while문의 작업을 위해)
+		
+	}//큐에 값 채움
 		
 
 	while(N>1) {
 		  
 		
-		  q.remove();//맨 위의 카드 버림
+		  queue.remove();//맨 위의 카드 버림
 		  
 		  
 		  //뒷장 카드를 맨 밑으로 보냄 
 		 
-		  q.add(q.peek()); 
-		  q.remove();
+		  queue.add(queue.peek()); 
+		  queue.remove();
 		  
 		  N--; 
 		  
 	}//카드가 한 장 남으면 종료
 		 	
-	System.out.println(q.peek());
+	System.out.println(queue.peek());
 
 	}
 }
