37c5fb62470938130bee3011194b37d551b6624a
은비 <67318165+eunbii0213@users.noreply.github.com>
2021-08-03 12:36:17 +0900

    Update 2178.java

--- 2178.java
+++ 2178.java
@@ -1,35 +1,40 @@
-import java.io.BufferedReader;
+mport java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-class Find{
+
+class Find2{
 	
 	public int x;
 	public int y;
 	
-	Find(int x, int y){
+	Find2(int x, int y){
 		this.x=x;
 		this.y=y;
-		
 	}
 	
 }
 
-public class UseStack{
+public class Main{
+	
 	
 	static int N;
 	static int M;
 	static int[][] map;
 	static boolean flag=true;
-	static boolean flag2;
-	static Stack<Find> stack = new Stack<>();
+	static Queue<Find2> queue = new LinkedList<>();
+	static Queue<Find2> queue2= new LinkedList<>();
 	static int rowIndex, colIndex;
-	static int[] dx = {0,1,0,-1};
-	static int[] dy = {1,0,-1,0};
-	static Find find;
+	static int[] dx = {0,1,-1,0};
+	static int[] dy = {1,0,0,-1};
+	static Find2 find;
+	static boolean flag2;
 	static int count=0;
+	
 
 
 	public static void main(String[] args) throws IOException {
@@ -62,93 +67,89 @@ public class UseStack{
 			
 			flag2=true;
 			
-			
 			map[rowIndex][colIndex]=2; //왔던길 2로 표시
-			stack.add(find = new Find(colIndex,rowIndex));
-	
-			if(rowIndex==N-1 && colIndex==M-1) {
-				while(!stack.isEmpty()) {
-					stack.pop();
-					count++;
-				}
-				
-				break Loop1;
-			}
+			count++;
 			
-				Loop2:
-				for(int i=0;i<4;i++) {
-					if(colIndex+dx[i]>=0 && rowIndex+dy[i]>=0 && colIndex+dx[i]<=M-1 && rowIndex+dy[i]<=N-1) {
-						
-					int x = colIndex+dx[i];
-					int y = rowIndex+dy[i];
+		
+				queue.add(find = new Find2(colIndex,rowIndex));
+				
+				if(rowIndex==N-1 && colIndex==M-1) {
 					
-					if((y==N-1)) {
+					while(!queue.isEmpty()) {
+						Find2 temp = queue.remove();
+						map[temp.y][temp.x]=3;
 						
-							int j=i+1;
-							while(j<4) {
-								
-								if( colIndex+dx[j]>=0 && rowIndex+dy[j]>=0 && colIndex+dx[j]<=M-1 && rowIndex+dy[j]<=N-1) {
-									
-									
-									if(map[rowIndex+dy[j]][colIndex+dx[j]]==1) {
-										colIndex+=dx[j];
-										rowIndex+=dy[j];
-										flag2=false;
-										continue Loop1;
-									}
-									
-									else {
-										j++;
-									}
-									
-								}
-								
-								else {
-									break;
-								}
-							}
-
-						
-					}//가려고 하는 곳이 맵의 (세로기준)마지막칸이고 다른 길로 갈 수 있다면 그 길로 가야 최단거리
-					
-					if(map[y][x]==1) {
-						colIndex=x;
-						rowIndex=y;
-						flag2=false;//지금 검사하는 칸 앞,뒤,양,옆 중 하나는 갈 수 있음 
-						continue Loop1;
-						
-					  	
-					   }
 					}
-					
-					
-				}
+					break Loop1;
+				}//미로 탈출 성공 
 				
-				
-				// 앞,뒤,양,옆 모두 막다른길이라면  pop
-				if(count>=1 && flag2) {
-					stack.pop();
-					count--;
-					
-					Find temp = stack.peek();
-					rowIndex= temp.y;
-					colIndex= temp.x;
+					Loop2:
+					for(int i=0;i<4;i++) {
+						
+						if(colIndex+dx[i]>=0 && rowIndex+dy[i]>=0 && colIndex+dx[i]<=M-1 && rowIndex+dy[i]<=N-1) {
+						
+						
+						int x = colIndex+dx[i];
+						int y = rowIndex+dy[i];
+						
+						
+						if(i==0 && rowIndex+dy[i]==N-1  && rowIndex+dy[1]<=N-1 && colIndex+dx[1]<=M-1) {
+							
+							if(map[rowIndex+dy[1]][colIndex+dx[1]]==1) {
+								x = colIndex+dx[1];
+								y = rowIndex+dy[1];
+							}
+							
+						}
+						
+						if(map[y][x]==1) {
+							colIndex=x;
+							rowIndex=y;
+							flag2=false;//지금 검사하는 칸 앞,뒤,양,옆 중 하나는 갈 수 있음 
+							continue Loop1;
+							
+						}
+						
+						
+						
+						
+					}
 				}
+			
+
+
+				// 앞,뒤,양,옆 모두 막다른길이라면 pop
+				if(flag && flag2) {
+					
+						 
+						queue.remove(find);
+						count--;
+						
+						
+							for(int i=0;i<count;i++){
+								if(i==count-1) {
+									Find2 temp = queue.peek();
+									rowIndex= temp.y;
+									colIndex= temp.x;
+								}//마지막값 저장해둠 
+								
+								queue.add(queue.peek());
+								queue.remove();
+								
+							}//for문 끝 
+					
+
+				}
+
 
 	}//while문 끝 
-	
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<M;j++) {
-				
-					System.out.print(map[i][j]+" ");
-
-			}
-			System.out.println();
-		}
-
+		
+		
 
 		System.out.println(count);
-	
 		
 	}
+	
+
 }
+
