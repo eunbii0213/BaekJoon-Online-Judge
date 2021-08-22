9187c2e376eaad8487aac37f5351b6f5beb3fcab
은비 <67318165+eunbii0213@users.noreply.github.com>
2021-08-22 18:53:20 +0900

    Create 1697.java

---
+++ 1697.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+
+
+public class Main{
+	
+	static int N;
+	static int M;
+	static int[] visit;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		 N = Integer.parseInt(st.nextToken());
+		 M = Integer.parseInt(st.nextToken());
+		 
+		 visit = new int[200000];
+		
+		
+		if(N==M) {
+			System.out.println("0");
+			return;
+			
+		}
+		
+		else {
+			bfs(N);
+		}
+
+		
+	}
+	
+	
+	public static void bfs(int N) {
+		
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(N);
+		visit[N]=0; //시작 위치 
+		
+		while(!queue.isEmpty()) {
+			
+			int index = queue.poll();
+			
+			
+			
+			for(int i=0;i<3;i++) {
+				int tx;
+				
+				if(i==0) {
+					 tx = index-1;
+				}
+				
+				else if(i==1) {
+					 tx = index+1;
+				}
+				
+				else {
+					 tx = index*2;
+				}
+				
+				
+				if(tx==M) {
+					System.out.println(visit[index]+1);
+					return;
+				}
+				
+				
+				//어떤 경우에는 M을 뛰어넘고 돌아오는게 가장 빠른 길일 수도 있다. 
+				if(tx>=0 && tx < visit.length && visit[tx]==0){
+					queue.add(tx);
+					
+					visit[tx]=visit[index]+1;
+					
+					
+					
+				}
+				
+				
+				
+			}
+			
+		
+			
+		}
+		
+		
+	}
+
+}
