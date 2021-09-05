5e69de9ec3697131977b9968648a62c3f9de2443
은비 <67318165+eunbii0213@users.noreply.github.com>
2021-09-05 22:21:32 +0900

    Update 5639.java

--- 5639.java
+++ 5639.java
@@ -2,55 +2,83 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
-    static class Node {
-        int num;
-        Node left, right;
-
-        Node(int num) {
-            this.num = num;
-        }
-
-        Node(int num, Node left, Node right) {
-            this.num = num;
-            this.left = left;
-            this.right = right;
-        }
-
-        void insert(int n) {
-            if (n < this.num) {
-                if (this.left == null)
-                    this.left = new Node(n);
-                else this.left.insert(n);
-            } else {
-                if (this.right == null)
-                    this.right = new Node(n);
-                else this.right.insert(n);
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        Node root = new Node(Integer.parseInt(br.readLine()));
-        String input;
-        while (true) {
-            input = br.readLine();
-            if (input == null || input.equals(""))
-                break;
-            root.insert(Integer.parseInt(input));
-        }
-
-        postOrder(root);
-    }
-
-    static void postOrder(Node node) {
-        if (node == null)
-            return;
-
-        postOrder(node.left);
-        postOrder(node.right);
-        System.out.println(node.num);
-    }
+class Node{
+	
+	int data;
+	Node leftChild;
+	Node rightChild;
+	
+	Node(int data){
+		this.data = data;
+		this.leftChild = null;
+		this.rightChild = null;
+	}
+	
+	//더 작으면 왼쪽자식, 더 크면 오른쪽 자식이 된다 
+	public void insert(int data) {
+		if(this.data>data) {
+			if(this.leftChild==null) {
+				this.leftChild = new Node(data);
+			}
+			
+			//이미 왼쪽자식이 있는 경우 왼쪽 자식의 왼쪽자식/오른쪽자식이 될 수 있는지 검사  
+			else {
+				this.leftChild.insert(data);
+			}
+		}
+		
+		else {
+			if(this.rightChild == null) {
+				this.rightChild = new Node(data);
+			}
+			
+			//이미 오른쪽자식이 있는 경우 왼쪽 자식의 왼쪽자식/오른쪽자식이 될 수 있는지 검사 
+			else {
+				this.rightChild.insert(data);
+			}
+		}
+	}
+	
 }
+public class Main {
+	
+	
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		Node root = new Node(Integer.parseInt(br.readLine()));
+		
+		while(true) {
+			
+			String input = br.readLine();
+			
+			//입력이 없을 때까지(공백 입력) 반복
+			if(input==null || input.equals("")) {
+				break;
+				
+			}
+			
+			else {
+				
+				root.insert(Integer.parseInt(input));
+			}
+		}//while문 끝. tree만들기 끝.
+		
+		postOrder(root);
+	}
+	
+	public static void postOrder(Node node) {
+		
+		if(node==null) {
+			return;
+		}
+		
+		postOrder(node.leftChild);
+		postOrder(node.rightChild);
+		System.out.println(node.data);
+	}
+
+}
+
