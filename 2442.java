fea6c1aae87087a500ea8d1f8eb8279889db8514
은비 <67318165+eunbii0213@users.noreply.github.com>
2021-11-24 07:49:13 +0900

    Create 2442.java

---
+++ 2442.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		
+		int count=1;
+		int blank = n-count;
+		
+		for(int i=0;i<n;i++) {
+			
+			for(int z=0;z<blank;z++) {
+				System.out.print(" ");
+			}
+			for(int j=0;j<count;j++) {
+				System.out.print("*");
+			}
+			
+			
+			count+=2;
+			blank--;
+			System.out.println();
+		}
+	}
+
+}
