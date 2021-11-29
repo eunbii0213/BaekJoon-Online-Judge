3159ccdf2f4852918cc4a565c368fafe2b57bfc3
은비 <67318165+eunbii0213@users.noreply.github.com>
2021-11-30 07:20:36 +0900

    Create 2446.java

---
+++ 2446.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int count=2*n-1;
+		int blank = 0;
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<blank;j++) {
+				System.out.print(" ");
+			}
+			
+			for(int z=0;z<count;z++) {
+				System.out.print("*");
+			}
+			blank++;
+			count-=2;
+			System.out.println();
+			
+		}
+		
+		 count = 3;
+		 blank = n-2;
+		
+		for(int i=0;i<n-1;i++) {
+			for(int j=0;j<blank;j++) {
+				System.out.print(" ");
+			}
+			
+			for(int z=0;z<count;z++) {
+				System.out.print("*");
+			}
+			blank--;
+			count+=2;
+			System.out.println();
+			
+		}
+
+	}
+
+}
