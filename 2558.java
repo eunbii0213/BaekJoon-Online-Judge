5929a64b0f1ef6df508d0c20e6ac79bf6fcd9e1a
은비 <67318165+eunbii0213@users.noreply.github.com>
2022-03-19 08:40:02 +0900

    Create 2558.java

---
+++ 2558.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.println(a+b);
+
+    }
+}
