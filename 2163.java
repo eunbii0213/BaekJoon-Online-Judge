92314061f1259bb3d6d34c67c51f146ccefc4449
은비 <67318165+eunbii0213@users.noreply.github.com>
2022-03-19 21:22:42 +0900

    Create 2163.java

---
+++ 2163.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        System.out.println( Math.min(n*(m-1)+(n-1),m*(n-1)+(m-1)));
+    }
+}
