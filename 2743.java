e7d5ba7b4020246b7a82e54e4e63b9f3865e95ea
은비 <67318165+eunbii0213@users.noreply.github.com>
2022-03-20 03:21:00 +0900

    Create 2743.java

---
+++ 2743.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str.length());
+    }
+}
