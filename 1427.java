f970820c59b34f3211190cad4f5f3e836e40ec79
은비 <67318165+eunbii0213@users.noreply.github.com>
2023-07-29 15:40:02 +0900

    Update 1427.java

--- 1427.java
+++ 1427.java
@@ -3,29 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Main{
-	
+public class Main {
 
-public static void main(String[] args) throws IOException {
-	
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	StringBuilder sb = new StringBuilder();
-	char[] arr = br.readLine().toCharArray();
-	
-	Arrays.sort(arr);
-	
-	for(int i=(arr.length-1);i>=0;i--) {
-		sb.append(arr[i]);
-	}
-	
-	System.out.println(sb);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(),"1|2|3|4|5|6|7|8|9|0", true);
+        Integer[] arr = new Integer[st.countTokens()];
+        int count = 0;
 
+        while (st.hasMoreTokens()) {
+            arr[count] = Integer.parseInt(st.nextToken());
+            count++;
+        }
 
-	}
+        Arrays.sort(arr, Collections.reverseOrder());
+        for (int i = 0; i < count; i++) {
+            System.out.print(arr[i]);
+        }
+    }
 }
