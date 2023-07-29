a6c6846d437268a3cea8714f27ec11e5909a00e7
은비 <67318165+eunbii0213@users.noreply.github.com>
2023-07-30 01:09:28 +0900

    Update 11724.java

--- 11724.java
+++ 11724.java
@@ -1,67 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.SQLOutput;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n; //vertex
-    static int m; //edge
-    static int arr[][]; //adjacent matrix
-    static int count=0; //connected component
-    static boolean visit[]; //check visit
- 
+    public static List<Integer>[] arr;
+    public static boolean[] visit;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n= Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[n+1][n+1];
-        visit = new boolean[n+1];
+        int n = Integer.parseInt(st.nextToken());
+        visit = new boolean[n + 1];
+        arr = new ArrayList[n + 1];
+        int m = Integer.parseInt(st.nextToken());
 
-        for(int i=0;i<m;i++){
+        for (int i = 0; i < n + 1; i++) {
+            arr[i] = new ArrayList<Integer>();
+        }
+
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
 
-            int y = Integer.parseInt(st.nextToken());
-            int x = Integer.parseInt(st.nextToken());
-            arr[y][x] = arr[x][y] = 1; //bi-directional
+            arr[s].add(e);
+            arr[e].add(s);
         }
 
-
-
-        for(int i=1;i<n+1;i++){
-           if(!visit[i]){
-               bfs(i);
-               count++;
-           }
-
-
+        int count = 0;
+        for (int i = 1; i < n + 1; i++) {
+            if (!visit[i]) {
+                count++;
+            }
+            DFS(i);
         }
+
         System.out.println(count);
-
-
-
     }
 
-    public static void bfs(int index){
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(index);
-        visit[index] = true;
-
-        while(!q.isEmpty()){
-            int now = q.poll();
-
-            for(int i=1;i<n+1;i++){
-                if(arr[now][i]==1 && !visit[i]){
-                    q.offer(i);
-                    visit[i]=true;
-                }
+    private static void DFS(final int i) {
+        if (visit[i]) {
+            return;
+        }
+        visit[i] = true;
+        for (Integer integer : arr[i]) {
+            if(visit[integer] == false){
+                DFS(integer);
             }
         }
     }
-
-
 }
