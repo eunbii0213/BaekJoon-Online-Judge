75dc4ef3eebba5ff0670bca9252069a8bc621ff5
은비 <67318165+eunbii0213@users.noreply.github.com>
2023-07-29 22:34:08 +0900

    Update 10989.java

--- 10989.java
+++ 10989.java
@@ -1,38 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
 
+public class Main {
 
+    public static void main(String[] args) throws IOException {
 
-public class Main{
-	
-		
-		public static void main(String[] args) throws IOException{
-			
-			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-			StringBuilder sb = new StringBuilder();
-			
-			int N; //N개의 수
-			N= Integer.parseInt(br.readLine());
-			
-			int[] arr = new int[N]; //N개의 배열 선언
-			
-			for(int i=0;i<N;i++) {
-				arr[i]=Integer.parseInt(br.readLine());
-			}//배열 입력 받음
-			
-			Arrays.sort(arr);
-			
-			for(int i=0;i<N;i++) {
-				sb.append(arr[i]).append('\n');
-			}
-			
-			System.out.println(sb);
-			
-		}
-	
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            array[i] = (Integer.parseInt(br.readLine()));
+        }
+
+        Arrays.sort(array);
+        for (int i = 0; i < n; i++) {
+            sb.append(array[i] + "\n");
+        }
+        System.out.println(sb);
+    }
 }
